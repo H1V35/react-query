fd8ade4bfa0ed9929cd700046aea8133e8fae462
H1V35 <higesjorge@gmail.com>
2024-02-20 22:00:28 +0100

    🔒 security: Add authorization

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -4,5 +4,8 @@ const API_URL = "https://api.github.com/repos/facebook/react";
 
 export const githubApi = axios.create({
   baseURL: API_URL,
-  headers: {},
+  headers: {
+    // Authorization:
+    //   "Bearer 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",
+  },
 });
