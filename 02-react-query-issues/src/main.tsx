ceb6d87823200592e2cc37c6fbf8805896449cb4
H1V35 <higesjorge@gmail.com>
2024-02-20 13:20:43 +0100

    🚀 setup: Initialize project

--- 02-react-query-issues/src/main.tsx
+++ 02-react-query-issues/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+//! Remover enable css source maps
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
