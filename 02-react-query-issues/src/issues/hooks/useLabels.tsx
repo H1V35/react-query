9a154426151c7487233ae9f3fe0e1073717cdc53
H1V35 <higesjorge@gmail.com>
2024-02-20 17:02:11 +0100

    ✨ feat: Add placeholderData to useQuery in useLabels custom hook

--- 02-react-query-issues/src/issues/hooks/useLabels.tsx
+++ 02-react-query-issues/src/issues/hooks/useLabels.tsx
@@ -3,6 +3,25 @@ import { githubApi } from "../../api/githubApi";
 import { sleep } from "../../helpers/sleep";
 import { Label } from "../interfaces/label";
 
+const placeholderLabels = [
+  {
+    id: 717031390,
+    node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+    url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+    name: "good first issue",
+    color: "6ce26a",
+    default: true,
+  },
+  {
+    id: 6344006318,
+    node_id: "LA_kwDOAJy2Ks8AAAABeiHarg",
+    url: "https://api.github.com/repos/facebook/react/labels/fb-exported",
+    name: "fb-exported",
+    color: "ededed",
+    default: false,
+  },
+];
+
 async function getLabels(): Promise<Label[]> {
   await sleep(2);
 
@@ -15,6 +34,7 @@ export function useLabels() {
     queryKey: ["labels"],
     queryFn: getLabels,
     staleTime: 1000 * 60 * 60,
+    placeholderData: placeholderLabels,
   });
 
   return { labelsQuery };
