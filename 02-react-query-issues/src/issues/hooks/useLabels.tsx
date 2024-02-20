ca0444fa8b474e567dbeb0d421affdcccdae0ac9
H1V35 <higesjorge@gmail.com>
2024-02-20 16:26:25 +0100

    🔄 refactor: Change default staleTime of useQuery to 1 hour in useLabels custom hook

--- 02-react-query-issues/src/issues/hooks/useLabels.tsx
+++ 02-react-query-issues/src/issues/hooks/useLabels.tsx
@@ -14,6 +14,7 @@ export function useLabels() {
   const labelsQuery = useQuery({
     queryKey: ["labels"],
     queryFn: getLabels,
+    staleTime: 1000 * 60 * 60,
   });
 
   return { labelsQuery };
