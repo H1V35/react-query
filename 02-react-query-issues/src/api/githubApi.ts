ad8060d655ff8018fb83115aa16a990d3b525f46
H1V35 <higesjorge@gmail.com>
2024-02-20 22:24:25 +0100

    🔒 security: Invalidate authorization token and use it as environment variable

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API_URL = "https://api.github.com/repos/facebook/react";
+const TOKEN = import.meta.env.VITE_GITHUB_BEARER_TOKEN;
 
 export const githubApi = axios.create({
   baseURL: API_URL,
   headers: {
-    // Authorization:
-    //   "Bearer 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",
+    Authorization: `Bearer ${TOKEN}`,
   },
 });
