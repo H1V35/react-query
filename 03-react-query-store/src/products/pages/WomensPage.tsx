7c207f0e5c68384d8bd2d30d468449508752de57
H1V35 <higesjorge@gmail.com>
2024-02-25 15:47:24 +0100

    ⚡ init: Create 03-react-query-store project using Vite and Initialize

---
+++ 03-react-query-store/src/products/pages/WomensPage.tsx
@@ -0,0 +1,11 @@
+import { ProductList } from "..";
+
+export function WomensPage() {
+  return (
+    <div className="flex-col">
+      <h1 className="text-2xl font-bold">Productos para mujeres</h1>
+
+      <ProductList />
+    </div>
+  );
+}
