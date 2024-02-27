378d1862805c855624e13b30254d15f0d2662319
H1V35 <higesjorge@gmail.com>
2024-02-27 16:38:02 +0100

    📦 feat: Add 'react-hook-form' library; Work on new product form with react-hook-form

--- 03-react-query-store/src/products/pages/NewProduct.tsx
+++ 03-react-query-store/src/products/pages/NewProduct.tsx
@@ -1,25 +1,96 @@
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { Button, Image, Input, Textarea } from '@nextui-org/react';
 
+interface FormInputs {
+  title: string;
+  price: number;
+  image: string;
+  description: string;
+  category: string;
+}
+
 export function NewProduct() {
+  const { control, handleSubmit } = useForm<FormInputs>({
+    defaultValues: {
+      title: '',
+      price: 0,
+      image: '',
+      description: '',
+      category: "men's clothing",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="w-full flex flex-col gap-4">
       <h1 className="text-2xl font-bold">New product</h1>
 
-      <form className="w-full">
+      <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col-reverse justify-around gap-4 sm:flex-row">
           <div className="flex flex-col w-full sm:w-1/2 gap-2">
-            <Input type="text" label="Titulo del producto" />
-            <Input type="number" label="Precio del producto" />
-            <Input type="url" label="Url del producto" />
-            <Textarea label="Descripcion del producto" />
-            <select className="rounded-md p-3 bg-gray-800 w-full">
-              <option value="men's clothing">Men's clothing</option>
-              <option value="women's clothing">Women's clothing</option>
-              <option value="jewelery">Jewelery</option>
-              <option value="electronics">Electronics</option>
-            </select>
+            <Controller
+              control={control}
+              name="title"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input value={field.value} onChange={field.onChange} type="text" label="Title" />
+              )}
+            />
 
-            <Button className="mt-2" color="primary">
+            <Controller
+              control={control}
+              name="price"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  value={field.value?.toString()}
+                  onChange={field.onChange}
+                  type="number"
+                  label="Price"
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="image"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input value={field.value} onChange={field.onChange} type="url" label="Image url" />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="description"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Textarea value={field.value} onChange={field.onChange} label="Description" />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="category"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <select
+                  value={field.value}
+                  onChange={field.onChange}
+                  className="rounded-md p-3 bg-gray-800 w-full"
+                >
+                  <option value="men's clothing">Men's clothing</option>
+                  <option value="women's clothing">Women's clothing</option>
+                  <option value="jewelery">Jewelery</option>
+                  <option value="electronics">Electronics</option>
+                </select>
+              )}
+            />
+
+            <Button type="submit" className="mt-2" color="primary">
               Create
             </Button>
           </div>
