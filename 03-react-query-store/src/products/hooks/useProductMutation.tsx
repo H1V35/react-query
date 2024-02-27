13921588878a1920fd6b08115fd3d27fafa8ed3b
H1V35 <higesjorge@gmail.com>
2024-02-27 18:24:08 +0100

    ✨ feat: Add invalidate tanstack queries to refetch on new product addition; Increase queryClient's staleTime to 1 hour

--- 03-react-query-store/src/products/hooks/useProductMutation.tsx
+++ 03-react-query-store/src/products/hooks/useProductMutation.tsx
@@ -1,11 +1,13 @@
 import { useMutation } from '@tanstack/react-query';
 import { createProduct } from '../services/actions';
+import { queryClient } from '../../lib/tanstack-query';
 
 export function useProductMutation() {
   const mutation = useMutation({
     mutationFn: createProduct,
-    onSuccess: () => console.log('Product created'),
-    onSettled: () => console.log('Product settle'),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['products', { filterKey: data.category }] });
+    },
   });
 
   return mutation;
