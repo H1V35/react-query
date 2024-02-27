6bf0ffbb568b050553de38f996988f839b68ff4e
H1V35 <higesjorge@gmail.com>
2024-02-27 13:13:11 +0100

    🐛 fix: Fix style bugs

--- 03-react-query-store/src/products/pages/NewProduct.tsx
+++ 03-react-query-store/src/products/pages/NewProduct.tsx
@@ -2,37 +2,34 @@ import { Button, Image, Input, Textarea } from '@nextui-org/react';
 
 export function NewProduct() {
   return (
-    <div className="w-full flex-col">
+    <div className="w-full flex flex-col gap-4">
       <h1 className="text-2xl font-bold">New product</h1>
 
       <form className="w-full">
-        <div className="flex justify-around items-center">
-          <div className="flex-col w-[500px]">
-            <Input className="mt-2" type="text" label="Titulo del producto" />
-            <Input className="mt-2" type="number" label="Precio del producto" />
-            <Input className="mt-2" type="url" label="Url del producto" />
-            <Textarea className="mt-2" label="Descripcion del producto" />
-            <select className="rounded-md p-3 mt-2 bg-gray-800 w-full">
+        <div className="flex flex-col-reverse justify-around gap-4 sm:flex-row">
+          <div className="flex flex-col w-full sm:w-1/2 gap-2">
+            <Input type="text" label="Titulo del producto" />
+            <Input type="number" label="Precio del producto" />
+            <Input type="url" label="Url del producto" />
+            <Textarea label="Descripcion del producto" />
+            <select className="rounded-md p-3 bg-gray-800 w-full">
               <option value="men's clothing">Men's clothing</option>
               <option value="women's clothing">Women's clothing</option>
               <option value="jewelery">Jewelery</option>
               <option value="electronics">Electronics</option>
             </select>
 
-            <br />
             <Button className="mt-2" color="primary">
               Create
             </Button>
           </div>
 
-          <div
-            className="bg-white rounded-2xl p-10 flex items-center"
-            style={{
-              width: '500px',
-              height: '600px',
-            }}
-          >
-            <Image src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" />
+          <div className="sm:w-1/2 max-h-[408px] items-center bg-white rounded-2xl p-10 flex justify-center">
+            <Image
+              src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+              width={300}
+              height={380}
+            />
           </div>
         </div>
       </form>
